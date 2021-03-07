01225b347310433033606bfd3aee985a3351d1e8
Derek Schwantner <derek@Dereks-Mac-mini.local>
2021-03-06 19:17:56 -0500

    edited file

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Profile from './components/Profile';
 import Feed from './components/Feed';
 import Archive from './components/Archive';
@@ -10,15 +10,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
+        <Router>
           <Link to='/archive'>Archive</Link>
           <Link to='/profile'>Profile</Link>
           <Link to='/feed'>Feed</Link>
 
-          <Route path='/profile' component={Profile} />
-          <Route path='/feed' component={Feed} />
-          <Route path='/archive' component={Archive} />
-        </BrowserRouter>
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/feed' component={Feed} />
+          <Route exact path='/archive' component={Archive} />
+        </Router>
       </header>
       
 
